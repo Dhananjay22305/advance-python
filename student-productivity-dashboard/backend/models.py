81a988b0ff3b2caae53d7be15dc4946e75cba923
Dhananjay22305 <bhoyar.dhananjay.22@gmail.com>
2026-01-27 20:45:57 +0530

    Add created_at attribute to Task class
    Added a created_at attribute to the Task class to track when a task is created.

--- student-productivity-dashboard/backend/models.py
+++ student-productivity-dashboard/backend/models.py
@@ -1,11 +1,12 @@
-# models.py
 
-# This class mimics a Database Table entry
+from datetime import datetime # <--- 1. Import the clock tool
+
 class Task:
     def __init__(self, id: int, title: str):
         self.id = id
         self.title = title
         self.done = False
+        self.created_at = datetime.now() 
 
 # This simulates our Database
 tasks_db = []
