0e92ac849747059dbd372b01b1d78269ab5090ca
Dhananjay22305 <bhoyar.dhananjay.22@gmail.com>
2026-01-27 20:37:54 +0530

    Implement logging in main.py with local time
    Added logging configuration to main.py and set local time for logs.

--- student-productivity-dashboard/backend/main.py
+++ student-productivity-dashboard/backend/main.py
@@ -1,5 +1,21 @@
+# main.py
+import logging
+import time 
+
+# 2. Force the logger to use Local Time (Computer time) instead of GMT
+logging.Formatter.converter = time.localtime 
+
 from fastapi import FastAPI
-from routes import tasks # <--- Import the new file
+from routes import tasks
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S" # Optional: Makes the date look cleaner
+)
+
+app = FastAPI()
 
 app = FastAPI()
 
