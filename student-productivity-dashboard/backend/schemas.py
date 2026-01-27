1174e18ae114fd5882d4e1b192f19065a0f26fd2
Dhananjay22305 <bhoyar.dhananjay.22@gmail.com>
2026-01-27 20:46:51 +0530

    Add created_at field to TaskResponse schema
    Added 'created_at' field to TaskResponse schema.

--- student-productivity-dashboard/backend/schemas.py
+++ student-productivity-dashboard/backend/schemas.py
@@ -1,15 +1,14 @@
-# schemas.py
-from pydantic import BaseModel
 
-# Base schema with shared attributes
+from pydantic import BaseModel
+from datetime import datetime 
+
 class TaskBase(BaseModel):
     title: str
 
-# Schema for receiving data (User only sends title)
 class TaskCreate(TaskBase):
     pass
 
-# Schema for returning data (We add id and done status)
 class TaskResponse(TaskBase):
     id: int
     done: bool
+    created_at: datetime 
